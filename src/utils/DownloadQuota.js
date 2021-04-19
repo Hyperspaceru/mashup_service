298d68520d5117c85170bcee909ae23a7cda8f65
eso <hyperspaceru@gmail.com>
2021-04-19 09:47:21 +0300

    add quota process and class

---
+++ src/utils/DownloadQuota.js
@@ -0,0 +1,10 @@
+export default class DownloadQuota {
+        constructor(){
+            this._downloadQuota = 20
+            this._quotaAboveAverageLikes = 10
+            this._quotaLowerAverageLikes = 10
+        }
+        quotaInit(){
+            
+        }
+}
